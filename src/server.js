f1b93e72bc7873e23eae55c2afaa228f4f87843e
Luis Palacios <lrpalaciosdev@gmail.com>
2019-01-25 17:49:46 -0600

    add express js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,9 @@
+const express = require('express');
 const geoFenceService = require('./geofence.service');
 
-async function test() {
-  // Test parameters
-  const origin = { lat: 12.1392374829317, lng: -86.286998949945 };
-  const destination = { lat: 12.1447169109533, lng: -86.296053417027 };
-  const waypoints = [
-    { lat: 12.1392374829317, lng: -86.286998949945 },
-    { lat: 12.1538583269418, lng: -86.2880882620811 },
-    { lat: 12.1411697341314, lng: -86.2921715900302 },
-    { lat: 12.1447169109533, lng: -86.296053417027 }];
-  const meters = 100;
+const app = express();
+const port = 3000;
 
-  // Invalid locations
-  // const currentUserLocation = { lat: 12.1465032, lng: -86.2903192 };
-  // const currentUserLocation = { lat: 12.147578499999998, lng: -86.2902213 };
-  // const currentUserLocation = { lat: 12.148578499999998, lng: -86.2902213 };
+app.get('/', (req, res) => res.send('Hello World'));
 
-  // Valid locations
-  const currentUserLocation = { lat: 12.1455785, lng: -86.2902213 };
-  // const currentUserLocation = { lat: 12.149578499999997, lng: -86.2902213 };
-
-  const isInGeoFence = await geoFenceService.isLocationInGeoFence(origin,
-    destination, waypoints, meters, currentUserLocation);
-  console.log(isInGeoFence);
-}
-test();
+app.listen(port, () => console.log(`App running on port ${port}`));
